c500794274cd61ceef253dfb9e716f0992180141
SundayDonghuLight <38806778+SundayDonghuLight@users.noreply.github.com>
2018-08-13 02:19:43 +0800

    ver2.0

--- Computer Organization 106_2/Homework 02/Find median from input.asm
+++ Computer Organization 106_2/Homework 02/Find median from input.asm
@@ -1,184 +1,70 @@
-# s0:a[]  s1:i  s2:Min  s3:M_position  s4:mid
+# s0:sequence[]  s1:i  s2:j  s3:k  s4:temp
 
 main:
 lui $s0, 0x1122
 
 add  $s1, $zero, $zero     #int i=0
-While:
+
+Inputdata:
 addi  $v0, $zero, 5        #cin>>a[i]
 syscall
-beq  $v0, $zero, EOF
+beq  $v0, $zero, EOF       #if(cin.eof()) break
 sll  $t0, $s1, 2
 add  $t0, $s0, $t0
 sw  $v0, 0($t0)
 addi  $s1, $s1, 1          #i++
-j  While
+j  Inputdata
+
 EOF:
-add  $t1, $zero, $zero     #int j=0
-addi  $t8, $s1, -1
-For1:
-slt  $t5, $t1, $t8         #for( ;j<i-1; )
-bne  $t5, $zero, L1
-j  Sort_End
-L1:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $s2, 0($t0)            #int Max=a[j]
-add  $s3, $t1, $zero       #int M_position=j
+addi $s2, $zero, 1         #j=1
 
-addi  $t2, $t1, 1          #int k=j+1
-For2:
-slt  $t5, $t2, $s1         #for( ;k<i; )
-bne  $t5, $zero, L2
-j  Search_End
-L2:
-sll  $t0, $t2, 2
-add  $t0, $s0, $t0
-lw  $t3, 0($t0)
-slt  $t5, $t3, $t2         #if(a[k]<Min)
-beq  $t5, $zero, L3
-add  $s2, $t3, $zero       #Min=a[k]
-add  $s3, $t2, $zero       #M_position=k
-addi  $t2, $t2, 1          #for( ; ;k++)
-L3:
-j  For2
-Search_End:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $t3, 0($t0)            #tem=a[j]
-sw  $s2, 0($t0)            #a[j]=Min
-sll  $t0, $s3, 2
-add  $t0, $s0, $t0
-sw  $t3, 0($t0)            #a[M_position]=tem
-addi  $t1, $t1, 1          #for( ; ;j++)
-j  For1
-Sort_End:
-add  $t1, $zero, $zero     #int j=0
-Print:
-slt  $t5, $t1, $s1         #for( ;j<i; )
-bne  $t5, $zero, L4
-j  Print_End
-L4:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $a0, 0($t0)
-addi  $v0, $zero, 1        #cout<<a[j]
-syscall
-addi  $t1, $t1, 1          #for( ; ;j++)
-j  Print
-Print_End:
-and  $t9, $t9, $zero
-addi  $t9, $t9, 1          #$t9:mask  [00...01]
-and  $t5, $s1, $t9         #if(i%2==0)
-bne  $t5, $zero, Else
-srl  $s4, $s1, 1           #int mid=i/2
-sll  $t0, $s4, 2
-add  $t0, $s0, $t0
-lw  $a0, 0($t0)
-addi  $v0, $zero, 1        #cout<<a[mid]
-syscall
-j  End
-Else:
-srl  $s4, $s1, 1           #int midL=i/2
-sll  $t0, $s4, 2
-add  $t0, $s0, $t0
-lw  $t1, 0($t0)
-lw  $t2, 4($t0)
-add  $t1, $t1, $t2
-srl  $a0, $t1, 1
-addi  $v0, $zero, 1
-syscall
-End:
-addi  $v0, $zero, 10
-syscall
+For:
+slt $t0, $s2, $s1          for(;j<i;)
+bne $zero, $t0, Sorted
+sll $t0, $s2, 2
+add $t0, $s0, $t0
+lw  $s4, 0($t0)
+addi $s3, $s2, -1
 
-
-add  $s1, $zero, $zero     #int i=0
 While:
-addi  $v0, $zero, 5        #cin>>a[i]
-syscall
-beq  $v0, $zero, EOF
-sll  $t0, $s1, 2
-add  $t0, $s0, $t0
-sw  $v0, 0($t0)
-addi  $s1, $s1, 1          #i++
-j  While
-EOF:
-add  $t1, $zero, $zero     #int j=0
-addi  $t8, $s1, -1
-For1:
-slt  $t5, $t1, $t8         #for( ;j<i-1; )
-bne  $t5, $zero, L1
-j  Sort_End
-L1:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $2, 0($t0)            #int Max=a[j]
-add  $s3, $t1, $zero       #int M_position=j
+slt $t0, $s3, $zero        #if(k<0) don't jump
+beq $t0, $zero ,Insert     #if(k>=0) break
+sll $t1, $s3, 2
+add $t1, $s0, $t0
+lw  $t2, 0($t1)            #$t2 <- sequence[k]
+slt $t0, $s4, $t1
+bne $t0, $zero ,Insert     #if(temp < sequence[k]) break
+sw  $t2, 4($t1)            #sequence[k+1] = sequence[k]
+addi $s3, $s3, -1          #k--
+j While
 
-addi  $t2, $t1, 1          #int k=j+1
-For2:
-slt  $t5, $t2, $s1         #for( ;k<i; )
-bne  $t5, $zero, L2
-j  Search_End
-L2:
-sll  $t0, $t2, 2
-add  $t0, $s0, $t0
-lw  $t3, 0($t0)
-slt  $t5, $t3, $t2         #if(a[k]<Min)
-beq  $t5, $zero, L3
-add  $s2, $t3, $zero       #Min=a[k]
-add  $s3, $t2, $zero       #M_position=k
-addi  $t2, $t2, 1          #for( ; ;k++)
-L3:
-j  For2
-Search_End:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $t3, 0($t0)            #tem=a[j]
-sw  $t2, 0($t0)            #a[j]=Min
-sll  $t0, $s3, 2
-add  $t0, $s0, $t0
-sw  $t3, 0($s0)            #a[M_position]=tem
-addi  $t1, $t1, 1          #for( ; ;j++)
-j  For1
-Sort_End:
-add  $t1, $zero, $zero     #int j=0
-Print:
-slt  $t5, $t1, $s1         #for( ;j<i; )
-bne  $t5, $zero, L4
-j  Print_End
-L4:
-sll  $t0, $t1, 2
-add  $t0, $s0, $t0
-lw  $a0, 0($t0)
-addi  $v0, $zero, 1        #cout<<a[j]
-syscall
-addi  $t1, $t1, 1          #for( ; ;j++)
-j  Print
-Print_End:
-and  $t9, $t9, $zero
-addi  $t9, $t9, 1          #$t9:mask  [00...01]
-and  $t5, $s1, $t9         #if(i%2==0)
-bne  $t5, $zero, Else
-srl  $s4, $s1, 1           #int mid=i/2
-sll  $t0, $s4, 2
-add  $t0, $s0, $t0
-lw  $a0, 0($t0)
-addi  $v0, $zero, 1        #cout<<a[mid]
+Insert:
+sll $t0, $s3, 2
+add $t0, $s0, $t0
+sw  $s4, 4($t0)            #sequence[k+1] = temp
+j For
+
+Sorted:
+addi $t0, $zero, 1
+and $t0, $t0, $s1
+beq $t0, $zero, Even
+srl $t1, $s1, 1
+sll  $t1, $t1, 2
+add  $t1, $s0, $t1
+lw  $a0, 0($t1)
+addi  $v0, $zero, 1        #cout<<sequence[mid]
 syscall
 j  End
-Else:
-srl  $s4, $s1, 1           #int midL=i/2
-sll  $t0, $s4, 2
+
+Even:
+srl $t0, $s1, 1
+sll  $t0, $t0, 2
 add  $t0, $s0, $t0
 lw  $t1, 0($t0)
-lw  $t2, 4($t0)
-add  $t1, $t1, $t2
-srl  $a0, $t1, 1
-addi  $v0, $zero, 1
-syscall
-End:
-addi  $v0, $zero, 10
+lw  $t2, -4($t0)
+add $t0, $t1, $t2
+srl $a0, $t0, 1
+addi  $v0, $zero, 1        #cout<<sequence[mid]
 syscall
 
+End
